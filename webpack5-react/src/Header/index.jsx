15e52d2ac9f2750e878abf0ba9bc7458d9c3944b
Vadim Vinogradov <vadim@vinogr.com>
2022-01-31 02:31:28 +0400

    Add react

---
+++ webpack5-react/src/Header/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import css from "@/Header/style.css";
+
+
+export const Header = ({children}) => <h1 style={{color: "gold"}}>{children}</h1>;
---
+++ webpack5-react/src/index.js
@@ -0,0 +1,6 @@
+import "@/style.css";
+import ReactDOM from "react-dom";
+import {Header} from '@/Header';
+
+
+ReactDOM.render(Header, document.querySelector("#app"));
