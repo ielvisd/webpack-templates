69e08471f60f77cf5ec85dc3fe7af85d32d0da61
Vadim Vinogradov <vadim@vinogr.com>
2022-01-31 01:48:26 +0400

    Update dependencies for Vue3

--- webpack5-simple-vue3/src/components/RootComponent/index.js
+++ webpack5-simple-vue3/src/components/RootComponent/index.js
@@ -1,5 +1,5 @@
 import Component1 from "@/components/Component1/index.vue";
-import Component2 from "@/components/Component1/index.vue";
+import Component2 from "@/components/Component2/index.vue";
 
 export default {
   components: {
